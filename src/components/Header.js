6efb546fb690057b6fae0b847ceda36e8ef71072
mihail-karabashev <mihailkarabashev@gmail.com>
2022-06-17 15:40:08 +0300

    Introduction to JSX syntax and React App Structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="App-header">
+            <p>
+                Hello, I am calling you from Header js
+            </p>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Main..js
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <main>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </main>
+    );
+}
+
+export default Main;
