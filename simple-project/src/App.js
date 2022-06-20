46ab80f1e21150e53332a972a7a05d59032ef942
mihail-karabashev <mihailkarabashev@gmail.com>
2022-06-20 16:04:34 +0300

    Few changes

---
+++ simple-project/src/App.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header";
+import WelcomeWorld from "./components/WelcomeWorld";
+import GameCatalog from "./components/GameCatalog/GameCatalog";
+import CreateGame from "./components/CreateGame"
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ErrorPage from "./components/ErrorPage";
+import GameDetails from "./components/GameDetails";
+
+function App() {
+  return (
+    <div id="box">
+      <Header />
+      <main id="main-content">
+        <Switch>
+          <Route path='/' exact component={WelcomeWorld} />
+          <Route path='/games' component={GameCatalog} />
+          <Route path='/create-game' component={CreateGame} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
